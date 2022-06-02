e7e6553326783e99553233f891903a6611e7599f
Lily Lam <lilylam@fb.com>
2022-06-02 05:53:30 -0700

    Accurately displaying types for each pokemon

--- scripts/app.js
+++ scripts/app.js
@@ -24,14 +24,29 @@ class Starters {
 
     makeStarters = function makeStarters(data){
         data.forEach(pokemon => {
+
+            //create cards
             $('<div>',{
+                class:"card",
                 id: pokemon.name,
                 text: pokemon.name,
             }).appendTo('.starters');
 
-            console.log(pokemon.type[0]);
+            //add types to cards
+                // dual-types
             if (pokemon.type[1] != null){
-                console.log(pokemon.type[1]);
+                $("body").find('#' + pokemon.name).append($('<div>', {
+                id: "pokemon-type",
+                text: pokemon.type[0] + " " + pokemon.type[1],
+                }));
+            }
+
+                // single-type
+            else {
+                $("body").find('#' + pokemon.name).append($('<div>', {
+                id: "pokemon-type",
+                text: pokemon.type[0],
+                }));
             }
         });
     }
