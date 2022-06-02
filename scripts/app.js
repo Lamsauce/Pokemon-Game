ee299ea5e5c0bdab4a8275d46f3a84a497f5c1f4
Lily Lam <lilylam@fb.com>
2022-06-02 09:15:23 -0700

    display sprites with pokemon, starter select, draft structure created

--- scripts/app.js
+++ scripts/app.js
@@ -2,17 +2,20 @@ const pokemon = [
     {
         name: "BULBASAUR",
         type: ["Grass", "Poison"],
-        attacks: ["Tackle", "Leer", "Vine Whip"]
+        attacks: ["Tackle", "Leer", "Vine Whip"],
+        sprite: "./images/bulbasaur.gif",
     },
     {
         name: "CHARMANDER",
         type: ["Fire", null],
-        attacks: ["Scratch", "Growl", "Ember"]
+        attacks: ["Scratch", "Growl", "Ember"],
+        sprite: "./images/charmander.gif",
     },
     {
         name: "SQUIRTLE",
         type: ["Water", null],
-        attacks: ["Tackle", "Withdraw", "Bubble"]
+        attacks: ["Tackle", "Withdraw", "Bubble"],
+        sprite: "./images/squirtle.gif",
     }
 ];
 
@@ -26,11 +29,20 @@ class Starters {
         data.forEach(pokemon => {
 
             //create cards
-            $('<div>',{
+            $('<div>', {
                 class:"card",
                 id: pokemon.name,
                 text: pokemon.name,
             }).appendTo('.starters');
+
+            //adds sprites to cards
+            $("body").find('#' + pokemon.name).prepend(
+            $('<img/>', {
+                class: "select-sprite",
+                id: pokemon.name + "-sprite",
+                src: pokemon.sprite,
+                })
+            );
 
             //add types to cards
                 // dual-types
